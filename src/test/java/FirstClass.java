10c7cc97c0cff2b84f912db1c7b210a35f097fbc
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-08 15:19:21 +0300

    08-10-23 First Seelenium Project

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class FirstClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","resources/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+    }
 }
